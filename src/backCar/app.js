c2fcf44eb8eee010b12058f49b4b0e1bcd44acac
RaulMY <35349012+RaulMY@users.noreply.github.com>
2018-04-14 09:33:42 -0500

    Update app.js

--- src/backCar/app.js
+++ src/backCar/app.js
@@ -16,7 +16,7 @@ const  corsOptions ={
 }
 app.use(require("cors")(corsOptions));
 
-require("mongoose").connect("mongodb://raulmy:raulmy@ds215019.mlab.com:15019/raulmy");
+require("mongoose").connect("mongodb:(Base de Datos)");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
